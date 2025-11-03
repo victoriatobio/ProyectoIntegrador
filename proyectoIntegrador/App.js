99a42b76fda49d58ad7a12954a47e01f46330e38
vickycastanio <vcastanoseippel@udesa.edu.ar>
2025-11-03 12:06:43 -0300

    correcion de navegacion

--- proyectoIntegrador/App.js
+++ proyectoIntegrador/App.js
@@ -8,10 +8,10 @@ import HomeMenu from "./src/Components/HomeMenu";
 export default function App() {
   return (
   <NavigationContainer>
-    <Stack.Navigator>      
-      <Stack.Screen name="HomeMenu" component={HomeMenu} options={{headerShown: false}}/>
+    <Stack.Navigator>   
       <Stack.Screen name="Register" component={Register} options={{headerShown: false}} />
       <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
+      <Stack.Screen name="HomeMenu" component={HomeMenu} options={{headerShown: false}}/>  
     </Stack.Navigator>
   </NavigationContainer>
   );
--- proyectoIntegrador/src/Components/HomeMenu.js
+++ proyectoIntegrador/src/Components/HomeMenu.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
-import Fontisto from '@expo/vector-icons/Fontisto';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import Home from "../Screens/Home";
 import Profile from "../Screens/Profile";
 import Comments from "../Screens/Comments";
@@ -14,16 +14,16 @@ const Tab = createBottomTabNavigator();
 function HomeMenu() {
     return(
  
-        <Tab.Navigator screenOptions={{ tabBarShowLabel: false}} >
+        <Tab.Navigator screenOptions={{ tabBarShowLabel: false, headerShown: false}} >
             
             <Tab.Screen name="Home" component={Home} 
                 options={{ tabBarIcon: () => <FontAwesome5 name="home" size={24} color="black" />  }} />
 
-            <Tab.Screen name="Profile" component={Profile}
-                options={{ tabBarIcon: () => <Fontisto name="comments" size={24} color="black" /> }}  />
+            <Tab.Screen name="Comments" component={Comments}
+                options={{ tabBarIcon: () => <Ionicons name="add-circle" size={24} color="black" /> }}  />   
 
-             <Tab.Screen name="Comments" component={Comments}
-                options={{ tabBarIcon: () => <AntDesign name="comment" size={24} color="black" /> }}  />   
+            <Tab.Screen name="Profile" component={Profile}
+                options={{ tabBarIcon: () => <Ionicons name="people" size={24} color="black" /> }}  />
 
         </Tab.Navigator>
 
