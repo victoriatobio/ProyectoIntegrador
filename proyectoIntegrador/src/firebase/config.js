ecb1d91dab68eec079da1c8aba7b53656f716d0e
Nicole de Leone <ndeleone@udesa.edu.ar>
2025-11-03 10:14:24 -0300

    Firebase creado y config conectado

---
+++ proyectoIntegrador/src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import firebase from "firebase"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAzJkw_mqWpVahJRWAs8Lz2MAC0a_0xsJw",
+    authDomain: "proyecto-integrador-ca94a.firebaseapp.com",
+    projectId: "proyecto-integrador-ca94a",
+    storageBucket: "proyecto-integrador-ca94a.firebasestorage.app",
+    messagingSenderId: "1059619989059",
+    appId: "1:1059619989059:web:90a6cd757be7eb264c56f6"
+  };
+
+app.initializeApp(firebaseConfig)
+
+export const auth = firebase.auth()
+export const storage = app.storage()
+export const db = app.firestore()  
+
+  
