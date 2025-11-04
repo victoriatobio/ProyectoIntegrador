637e9a1013d98c2e717d817ac5a74ca796486362
vickycastanio <vcastanoseippel@udesa.edu.ar>
2025-11-04 13:02:08 -0300

    creacion de el formulario de nuevo post

--- proyectoIntegrador/src/Screens/PostComments.js
+++ proyectoIntegrador/src/Screens/PostComments.js
@@ -1,23 +1,46 @@
-import {React} from 'react';
-import { Text } from 'react-native';
-import { View } from 'react-native';
-import { StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import { Text, TextInput, View, StyleSheet, Pressable } from 'react-native';
 
-function PostComments() {
-  return (
-    <View>
-      <Text style={styles.titulo} >Crear nuevo post</Text>
-    </View>
-  );
+class PostComments extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      comentario: "",
+    };
+  }
+
+  onSubmit() {
+    console.log('Comentario:', this.state.comentario);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text style={styles.titulo}>Crea un nuevo post</Text>
+
+        <TextInput
+          keyboardType="default"
+          placeholder="Crea un nuevo Post..."
+          onChangeText={text => this.setState({ comentario: text })}
+          value={this.state.comentario}
+        />
+
+        <Pressable onPress={() => this.onSubmit()}>
+          <Text>Submit</Text>
+        </Pressable>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-      titulo: {
-        fontSize: 30,
-        fontWeight: 'bold',
-        marginBottom: 20,
-        textAlign: 'center',
-    }
+  titulo: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  }
 });
 
 export default PostComments;
