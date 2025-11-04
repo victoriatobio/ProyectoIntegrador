7ce70d40b1d939f762f530f0a13458bd0347bd92
vickycastanio <vcastanoseippel@udesa.edu.ar>
2025-11-04 13:08:04 -0300

    segui con el punto de crear post

--- proyectoIntegrador/src/Screens/PostComments.js
+++ proyectoIntegrador/src/Screens/PostComments.js
@@ -1,34 +1,65 @@
 import React, { Component } from 'react';
 import { Text, TextInput, View, StyleSheet, Pressable } from 'react-native';
+import { db, auth } from "../firebase/config";
 
 class PostComments extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      comentario: "",
+      description: "",
+      loading: false,
+      error: "",
     };
   }
 
   onSubmit() {
-    console.log('Comentario:', this.state.comentario);
+    this.setState({
+      loading: true,
+      error: "",
+    });
+
+    db.collection("posts").add({
+        owner: auth.currentUser.email,
+        description: this.state.description,
+        createdAt: Date.now(),
+      })
+      .then(() => {
+        this.setState({
+          description: "",
+          loading: false,
+        });
+        this.props.navigation.navigate("Home");
+      })
+      .catch(() => {
+        this.setState({
+          error: "Error al crear el post",
+          loading: false,
+        });
+      });
   }
 
-  render() {
+render() {
     return (
-      <View>
-        <Text style={styles.titulo}>Crea un nuevo post</Text>
+      <View style={styles.container}>
+        <Text style={styles.titulo}>Crea un nuevo Post</Text>
 
         <TextInput
-          keyboardType="default"
-          placeholder="Crea un nuevo Post..."
-          onChangeText={text => this.setState({ comentario: text })}
-          value={this.state.comentario}
+          style={styles.textInput}
+          placeholder="Agrega un nuevo post"
+          value={this.state.description}
+          onChangeText={(text) => this.setState({ description: text })}
         />
 
-        <Pressable onPress={() => this.onSubmit()}>
-          <Text>Submit</Text>
-        </Pressable>
+        <Text style={styles.error}>{this.state.error}</Text>
+
+        {this.state.loading ? (
+          <ActivityIndicator size="large" color="#0df055ff" />
+        ) : (
+          <Pressable style={styles.boton} onPress={() => this.onSubmit()}>
+            <Text style={styles.textoBoton}>Publicar</Text>
+          </Pressable>
+        )}
       </View>
     );
   }
