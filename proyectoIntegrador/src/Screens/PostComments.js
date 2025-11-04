814665a5cf4d27a38ec17fb0fcc58543f7dbbd20
vickycastanio <vcastanoseippel@udesa.edu.ar>
2025-11-04 13:17:13 -0300

    crear post es funcional

--- proyectoIntegrador/src/Screens/PostComments.js
+++ proyectoIntegrador/src/Screens/PostComments.js
@@ -14,50 +14,52 @@ class PostComments extends Component {
   }
 
   onSubmit() {
-    this.setState({
-      loading: true,
-      error: "",
-    });
+ 
+  if (!auth.currentUser) {
+    this.setState({ error: "Debes estar logueado para publicar." });
+  } 
+  else if (this.state.description === "") {
+    this.setState({ error: "El post no puede estar vacío." });
+  } 
+  else {
+    this.setState({ loading: true, error: "" });
 
-    db.collection("posts").add({
+    db.collection("posts")
+      .add({
         owner: auth.currentUser.email,
         description: this.state.description,
         createdAt: Date.now(),
+        likes: []
       })
       .then(() => {
-        this.setState({
-          description: "",
-          loading: false,
-        });
+        this.setState({ description: "", loading: false });
         this.props.navigation.navigate("Home");
       })
       .catch(() => {
-        this.setState({
-          error: "Error al crear el post",
-          loading: false,
-        });
+        this.setState({ error: "Error al crear el post", loading: false });
       });
   }
+}
+
 
 render() {
     return (
-      <View style={styles.container}>
+      <View >
         <Text style={styles.titulo}>Crea un nuevo Post</Text>
 
         <TextInput
-          style={styles.textInput}
           placeholder="Agrega un nuevo post"
           value={this.state.description}
           onChangeText={(text) => this.setState({ description: text })}
         />
 
-        <Text style={styles.error}>{this.state.error}</Text>
+        <Text>{this.state.error}</Text>
 
         {this.state.loading ? (
           <ActivityIndicator size="large" color="#0df055ff" />
         ) : (
-          <Pressable style={styles.boton} onPress={() => this.onSubmit()}>
-            <Text style={styles.textoBoton}>Publicar</Text>
+          <Pressable  onPress={() => this.onSubmit()}>
+            <Text>Publicar</Text>
           </Pressable>
         )}
       </View>
