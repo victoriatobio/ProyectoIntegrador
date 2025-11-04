998acca40c29b85a5a62639c84786cb431568039
vickycastanio <vcastanoseippel@udesa.edu.ar>
2025-11-04 13:40:45 -0300

    ccs del componente post

--- proyectoIntegrador/src/Components/Post.js
+++ proyectoIntegrador/src/Components/Post.js
@@ -4,14 +4,36 @@ import { View, Text, StyleSheet } from 'react-native';
 class Post extends Component {
   render() {
     return (
-      <View >
-        <Text >{this.props.postData.owner}</Text>
-        <Text>{this.props.postData.description}</Text>
+      <View style={styles.card} >
+        <Text style={styles.owner} >{this.props.postData.owner}</Text>
+        <Text style={styles.description} >{this.props.postData.description}</Text>
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: '#E6ECF0',
+  },
+  owner: {
+    color: '#1DA1F2',
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  description: {
+    fontSize: 16,
+    color: '#14171A',
+    marginBottom: 5,
+  }
+});
 
 
 export default Post;
