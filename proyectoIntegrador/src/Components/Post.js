cdc23cf8eb0729a607d901dccf50f9846a84801d
Nicole de Leone <ndeleone@udesa.edu.ar>
2025-11-09 20:39:53 -0300

    intento de que solo pueda dar like una vez

--- proyectoIntegrador/src/Components/Post.js
+++ proyectoIntegrador/src/Components/Post.js
@@ -10,10 +10,14 @@ class Post extends Component {
     super(props);
     this.state = {
       fav: false,
+      listaDeYaLikearon: [],
+      texto:""
     };
   }
 
+  
   componentDidMount() {
+    
 
     if (this.props.postData.data.likes.includes(auth.currentUser.email)) {
       this.setState({fav:true})
@@ -22,31 +26,42 @@ class Post extends Component {
 
   }
   agregarEmailFavoritos() {
-    if (this.props.postData.data.likes.includes(auth.currentUser.email)){
-      db.collection('posts')
-            .doc(this.props.postData.id)
-            .update({
-               likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
+    let lista = []
 
-            })
-            .then(()=>{
-                this.setState({
-                    fav:false
-                })
-            })
-
-  } else{
-      db.collection('posts')
-      .doc(this.props.postData.id)
-      .update({
-         likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email)
-      })
-      .then(()=>{
-        this.setState({fav:true
+    if (this.state.listaDeYaLikearon.includes(auth.currentUser.email)){
+      this.setState({texto:"Ya le diste like a este post"})
+    } else {
+      if (this.props.postData.data.likes.includes(auth.currentUser.email)){
+        db.collection('posts')
+              .doc(this.props.postData.id)
+              .update({
+                 likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
+  
+              })
+              .then(()=>{
+                  this.setState({
+                      fav:false
+                  })
+              })
+  
+      } else{
+          db.collection('posts')
+          .doc(this.props.postData.id)
+          .update({
+            likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email)
           })
-      })
-
-  }
+          .then(()=>{
+            lista.push(auth.currentUser.email);
+            this.setState({
+              fav:true,
+              listaDeYaLikearon: lista
+              
+              })
+          })
+    
+      }
+      }
+  
   }
 
 
